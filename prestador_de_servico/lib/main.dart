452a023d68cc2bdba07c81f316ef7075e48e7f02
victorfidelis <fideliscorrea.victor@gmail.com>
2024-09-16 23:44:16 -0300

    Firebase config

--- prestador_de_servico/lib/main.dart
+++ prestador_de_servico/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:prestador_de_servico/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
