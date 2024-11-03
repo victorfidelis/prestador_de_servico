0681658728ec4fa3435fdfd94989dedf7d962fbf
victorfidelis <fideliscorrea.victor@gmail.com>
2024-11-03 16:25:19 -0300

    Unit test refactoring for all auth controllers (sign in, create and reset password)

--- prestador_de_servico/test/app/controllers/auth/password_reset_controller_test.dart
+++ prestador_de_servico/test/app/controllers/auth/password_reset_controller_test.dart
@@ -1,50 +1,70 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:prestador_de_servico/app/controllers/auth/password_reset_controller.dart';
+import 'package:prestador_de_servico/app/models/user/user.dart';
 import 'package:prestador_de_servico/app/services/auth/auth_service.dart';
+import 'package:prestador_de_servico/app/shared/either/either.dart';
+import 'package:prestador_de_servico/app/shared/failure/failure.dart';
 import 'package:prestador_de_servico/app/states/auth/password_reset_state.dart';
 
 import '../../../helpers/auth/mock_auth_repository.dart';
-import '../../../helpers/constants/user_constants.dart';
 import '../../../helpers/user/mock_user_repository.dart';
 
 void main() {
   late PasswordResetController passwordResetController;
 
-  setUpAll(
+  late User user1;
+
+  setUpValues() {
+    user1 = User(
+      email: 'victor@gmail.com',
+      name: 'Victor',
+      surname: 'Fidelis Correa',
+    );
+  }
+
+  setUp(
     () {
-      // As configurações abaixo criam os comportamentos mockados para diferentes
-      // situações relacionadas a autenticação e cadastro de usuário, além de instânciar
-      // os mocks públicos mockAuthRepository e mockUserRepository
       setUpMockAuthRepository();
       setUpMockUserRepository();
       AuthService authService = AuthService(
         authRepository: mockAuthRepository,
         userRepository: mockUserRepository,
       );
-      passwordResetController =
-          PasswordResetController(authService: authService);
+      passwordResetController = PasswordResetController(authService: authService);
+      setUpValues();
     },
   );
 
-  test(
-    '''Ao tentar enviar um email de recuperação de senha sem conexão com a internet
-    o estado da controller deve ser alterado para ErrorPasswordResetEmail''',
-    () async {
-      await passwordResetController.sendPasswordResetEmail(
-          email: userNoNetworkConection.email);
+  group(
+    'sendPasswordResetEmail',
+    () {
+      test(
+        '''Deve definir o estado como ErrorPasswordResetEmail quando não existir acesso a internet''',
+        () async {
+          const failureMessage = 'Teste de falha';
+          when(mockAuthRepository.sendPasswordResetEmail(email: user1.email))
+              .thenAnswer((_) async => Either.left(NetworkFailure(failureMessage)));
 
-      expect(passwordResetController.state is ErrorPasswordResetEmail, isTrue);
-    },
-  );
+          await passwordResetController.sendPasswordResetEmail(email: user1.email);
 
-  test(
-    '''Ao tentar enviar um email de recuperação de senha devidamente conectado a internet
-    o estado da controller deve ser alterado para PasswordResetEmailSentSuccess''',
-    () async {
-      await passwordResetController.sendPasswordResetEmail(
-          email: userValidToSendResetPasswordEmail.email);
+          expect(passwordResetController.state is ErrorPasswordResetEmail, isTrue);
+          final state = (passwordResetController.state as ErrorPasswordResetEmail);
+          expect(state.message, equals(failureMessage));
+        },
+      );
 
-      expect(passwordResetController.state is PasswordResetEmailSentSuccess, isTrue);
+      test(
+        '''Deve definir o estado como PasswordResetEmailSentSuccess quando a solicitação for válida''',
+        () async {
+          when(mockAuthRepository.sendPasswordResetEmail(email: user1.email))
+              .thenAnswer((_) async => Either.right(unit));
+
+          await passwordResetController.sendPasswordResetEmail(email: user1.email);
+
+          expect(passwordResetController.state is PasswordResetEmailSentSuccess, isTrue);
+        },
+      );
     },
   );
 }
--- prestador_de_servico/test/app/controllers/auth/sign_in_controller_test.dart
+++ prestador_de_servico/test/app/controllers/auth/sign_in_controller_test.dart
@@ -1,20 +1,32 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:prestador_de_servico/app/controllers/auth/sign_in_controller.dart';
+import 'package:prestador_de_servico/app/models/user/user.dart';
 import 'package:prestador_de_servico/app/services/auth/auth_service.dart';
+import 'package:prestador_de_servico/app/shared/either/either.dart';
+import 'package:prestador_de_servico/app/shared/failure/failure.dart';
 import 'package:prestador_de_servico/app/states/auth/sign_in_state.dart';
 
 import '../../../helpers/auth/mock_auth_repository.dart';
-import '../../../helpers/constants/user_constants.dart';
 import '../../../helpers/user/mock_user_repository.dart';
 
 void main() {
   late SignInController signInController;
 
+  late User user1;;
+
+  setUpValues() {
+    user1 = User(
+      email: 'victor@gmail.com',
+      name: 'Victor',
+      surname: 'Fidelis Correa',
+      password: '123466',
+      confirmPassword: '123466',
+    );
+  }
+
   setUpAll(
     () {
-      // As configurações abaixo criam os comportamentos mockados para diferentes
-      // situações relacionadas a autenticação e cadastro de usuário, além de instânciar
-      // os mocks públicos mockAuthRepository e mockUserRepository
       setUpMockAuthRepository();
       setUpMockUserRepository();
       AuthService authService = AuthService(
@@ -22,128 +34,157 @@ void main() {
         userRepository: mockUserRepository,
       );
       signInController = SignInController(authService: authService);
+      setUpValues();
     },
   );
 
-  test(
-    '''Ao tentar logar sem informar o email o estado do controller deve ser 
-    alterado para SignInError e conter uma mensagem no campo de email''',
-    () async {
-      await signInController.signInEmailPassword(
-        email: userWithoutEmail.email,
-        password: userWithoutEmail.password,
+  group(
+    'signInEmailPassword',
+    () {
+      test(
+        '''Deve definir o estado como SignInError e definir uma mensagem no campo "emailMessage" 
+        quando o campo "email" estiver vazio''',
+        () async { 
+          await signInController.signInEmailPassword(
+            email: '',
+            password: user1.password,
+          );
+
+          expect(signInController.state is SignInError, isTrue);
+          final state = signInController.state as SignInError;
+          expect(state.emailMessage, isNotNull);
+        },
       );
 
-      expect(signInController.state is SignInError, isTrue);
-      final state = signInController.state as SignInError;
-      expect(state.emailMessage, isNotNull);
-    },
-  );
+      test(
+        '''Deve definir o estado como SignInError e definir uma mensagem no campo "passwordMessage" 
+        quando o campo "password" estiver vazio''',
+        () async {
+          await signInController.signInEmailPassword(
+            email: user1.email,
+            password: '',
+          );
 
-  test(
-    '''Ao tentar logar sem informar a senha o estado do controller deve ser 
-    alterado para SignInError e conter uma mensagem no campo de senha''',
-    () async {
-      await signInController.signInEmailPassword(
-        email: userWithoutPassword.email,
-        password: userWithoutPassword.password,
+          expect(signInController.state is SignInError, isTrue);
+          final state = signInController.state as SignInError;
+          expect(state.passwordMessage, isNotNull);
+        },
       );
 
-      expect(signInController.state is SignInError, isTrue);
-      final state = signInController.state as SignInError;
-      expect(state.passwordMessage, isNotNull);
-    },
-  );
+      test(
+        '''Deve definir o estado como SignInError e definir uma mensagem no campo "genericMessage" 
+        quando não existir acesso a internet''',
+        () async {
+          const failureMessage = 'Teste de falha';
+          when(mockUserRepository.getByEmail(email: user1.email))
+              .thenAnswer((_) async => Either.left(NetworkFailure(failureMessage)));
 
-  test(
-    '''Ao tentar logar sem estar conectado a internet o estado do controller deve ser
-    alterado para SignInError e conter uma mensagem no campo genérico''',
-    () async {
-      await signInController.signInEmailPassword(
-        email: userNoNetworkConection.email,
-        password: userNoNetworkConection.password,
+          await signInController.signInEmailPassword(
+            email: user1.email,
+            password: user1.password,
+          );
+
+          expect(signInController.state is SignInError, isTrue);
+          final state = signInController.state as SignInError;
+          expect(state.genericMessage, equals(failureMessage));
+        },
       );
 
-      expect(signInController.state is SignInError, isTrue);
-      final state = signInController.state as SignInError;
-      expect(state.genericMessage, isNotNull);
-    },
-  );
+      test(
+        '''Deve definir o estado como SignInError e definir uma mensagem no campo "genericMessage" 
+        quando o campo "email" não estiver cadastrado''',
+        () async {
+          const failureMessage = 'Teste de falha';
+          when(mockUserRepository.getByEmail(email: user1.email))
+              .thenAnswer((_) async => Either.left(UserNotFoundFailure(failureMessage)));
 
-  test(
-    '''Ao tentar logar com um email não cadatrado o estado do controller deve ser
-    alterado para SignInError e conter uma mensagem no campo genérico''',
-    () async {
-      await signInController.signInEmailPassword(
-        email: userNotFoud.email,
-        password: userNotFoud.password,
+          await signInController.signInEmailPassword(
+            email: user1.email,
+            password: user1.password,
+          );
+
+          expect(signInController.state is SignInError, isTrue);
+          final state = signInController.state as SignInError;
+          expect(state.genericMessage, equals(failureMessage));
+        },
       );
 
-      expect(signInController.state is SignInError, isTrue);
-      final state = signInController.state as SignInError;
-      expect(state.genericMessage, isNotNull);
-    },
-  );
+      test(
+        '''Deve definir o estado como SignInError e definir uma mensagem no campo "genericMessage" 
+        quando o campo "password" não estiver correto''',
+        () async {
+          const failureMessage = 'Teste de falha';
+          when(mockUserRepository.getByEmail(email: user1.email)).thenAnswer((_) async => Either.right(user1));
+          when(mockAuthRepository.signInEmailPassword(email: user1.email, password: user1.password))
+              .thenAnswer((_) async => Either.left(InvalidCredentialFailure(failureMessage)));
 
-  test(
-    '''Ao tentar logar com uma senha incorreta o estado do controller deve ser
-    alterado para SignInError e conter uma mensagem no campo genérico''',
-    () async {
-      await signInController.signInEmailPassword(
-        email: userInvalidCredentials.email,
-        password: userInvalidCredentials.password,
+          await signInController.signInEmailPassword(
+            email: user1.email,
+            password: user1.password,
+          );
+
+          expect(signInController.state is SignInError, isTrue);
+          final state = signInController.state as SignInError;
+          expect(state.genericMessage, equals(failureMessage));
+        },
       );
 
-      expect(signInController.state is SignInError, isTrue);
-      final state = signInController.state as SignInError;
-      expect(state.genericMessage, isNotNull);
-    },
-  );
+      test(
+        '''Deve definir o estado como SignInError e definir uma mensagem para o campo "genericMessage"
+        quando o campo "email" não estiver confirmado''',
+        () async {
+          const failureMessage = 'Teste de falha';
+          when(mockUserRepository.getByEmail(email: user1.email)).thenAnswer((_) async => Either.right(user1));
+          when(mockAuthRepository.signInEmailPassword(email: user1.email, password: user1.password))
+              .thenAnswer((_) async => Either.left(EmailNotVerifiedFailure(failureMessage)));
+          when(mockAuthRepository.sendEmailVerificationForCurrentUser()).thenAnswer((_) async => Either.right(unit));
 
-  test(
-    '''Ao tentar logar em uma conta que não possui email validado o estado do 
-    controller deve ser alterado para SignInError e conter uma mensagem 
-    no campo genérico''',
-    () async {
-      await signInController.signInEmailPassword(
-        email: userEmailNotVerified.email,
-        password: userEmailNotVerified.password,
+          await signInController.signInEmailPassword(email: user1.email, password: user1.password);
+
+          expect(signInController.state is SignInError, isTrue);
+          final state = signInController.state as SignInError;
+          expect(state.genericMessage, isNotEmpty);
+        },
       );
 
-      expect(signInController.state is SignInError, isTrue);
-      final state = signInController.state as SignInError;
-      expect(state.genericMessage, isNotNull);
-    },
-  );
+      test(
+        '''Deve definir o estado como SignInError e definir uma mensagem para o campo "genericMessage"
+        quando muitas tentativas inválidas forem executadas''',
+        () async {
+          const failureMessage = 'Teste de falha';
+          when(mockUserRepository.getByEmail(email: user1.email)).thenAnswer((_) async => Either.right(user1));
+          when(mockAuthRepository.signInEmailPassword(email: user1.email, password: user1.password))
+              .thenAnswer((_) async => Either.left(TooManyRequestsFailure(failureMessage)));
 
-  test(
-    '''Ao tentar logar em uma conta após muitas tentativa inválidas o estado do 
-    controller deve ser alterado para SignInError e conter uma mensagem 
-    no campo genérico''',
-    () async {
-      await signInController.signInEmailPassword(
-        email: userTooManyRequests.email,
-        password: userTooManyRequests.password,
+          await signInController.signInEmailPassword(
+            email: user1.email,
+            password: user1.password,
+          );
+
+          expect(signInController.state is SignInError, isTrue);
+          final state = signInController.state as SignInError;
+          expect(state.genericMessage, equals(failureMessage));
+        },
       );
 
-      expect(signInController.state is SignInError, isTrue);
-      final state = signInController.state as SignInError;
-      expect(state.genericMessage, isNotNull);
-    },
-  );
+      test(
+        '''Deve definir o estado como SignInSuccess e definir o usuário no campo "user"
+        quando o "email" e "password" estiverem válidos''',
+        () async {
+          when(mockUserRepository.getByEmail(email: user1.email)).thenAnswer((_) async => Either.right(user1));
+          when(mockAuthRepository.signInEmailPassword(email: user1.email, password: user1.password))
+              .thenAnswer((_) async => Either.right(unit));
 
-  test(
-    '''Ao tentar logar em uma conta válida com a credenciais válidas o estado do 
-    controller deve ser alterado para SignInSuccess com o respectivo usuário logado''',
-    () async {
-      await signInController.signInEmailPassword(
-        email: userValidToSignIn.email,
-        password: userValidToSignIn.password,
+          await signInController.signInEmailPassword(
+            email: user1.email,
+            password: user1.password,
+          );
+
+          expect(signInController.state is SignInSuccess, isTrue);
+          final state = signInController.state as SignInSuccess;
+          expect(state.user, equals(user1));
+        },
       );
-
-      expect(signInController.state is SignInSuccess, isTrue);
-      final state = signInController.state as SignInSuccess;
-      expect(state.user, equals(userValidToSignIn));
     },
   );
 }
